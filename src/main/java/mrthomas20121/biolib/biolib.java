20c4eba514b3b145dab01aed0ac39b88cb4f40b4
mrthomas20121 <thomas77300@gmail.com>
2020-06-28 17:01:15 +0200

    stuff

---
+++ src/main/java/mrthomas20121/biolib/biolib.java
@@ -0,0 +1,35 @@
+package mrthomas20121.biolib;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = biolib.MODID, name = biolib.NAME, version = biolib.VERSION,
+        dependencies = "required-after:forge@[14.23.5.2847,);"
+                + "required-after:mantle@[1.12-1.3.3.55,);"
+                + "required-after:tconstruct@[1.12.2-2.13.0.183,);")
+public class biolib {
+    public static final String MODID = "biolib";
+    public static final String NAME = "Bio Library";
+    public static final String VERSION = "1.0.4";
+
+    public static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+        logger = event.getModLog();
+    }
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    }
+}
