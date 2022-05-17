6c2b102f4c579ab6140ea46a73aedcecab894866
mrthomas20121 <thomas77300@gmail.com>
2022-05-17 11:58:37 +0200

    better ore config

--- src/main/java/mrthomas20121/biolib/worldgen/OreConfig.java
+++ src/main/java/mrthomas20121/biolib/worldgen/OreConfig.java
@@ -2,15 +2,16 @@ package mrthomas20121.biolib.worldgen;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import java.util.Locale;
+
 public class OreConfig {
     public ForgeConfigSpec.BooleanValue enabled;
-    public ForgeConfigSpec.IntValue minY;
-    public ForgeConfigSpec.IntValue maxY;
-    public ForgeConfigSpec.IntValue count;
-    public ForgeConfigSpec.IntValue size;
+    public ForgeConfigSpec.ConfigValue<Integer> minY;
+    public ForgeConfigSpec.ConfigValue<Integer> maxY;
+    public ForgeConfigSpec.ConfigValue<Integer> count;
+    public ForgeConfigSpec.ConfigValue<Integer> size;
 
-    public OreConfig(ForgeConfigSpec.Builder builder) {
-    }
+    private OreConfig(ForgeConfigSpec.Builder builder) {}
 
     public boolean isEnabled() {
         return enabled.get();
@@ -30,5 +31,21 @@ public class OreConfig {
 
     public int getMinY() {
         return minY.get();
+    }
+
+    public static OreConfig create(ForgeConfigSpec.Builder builder, int minY, int maxY, int count, int size) {
+        return new BasicOreConfig(builder, minY, maxY, count, size);
+    }
+
+    private static class BasicOreConfig extends OreConfig {
+
+        private BasicOreConfig(ForgeConfigSpec.Builder builder, int minY, int maxY, int count, int size) {
+            super(builder);
+            this.enabled = builder.comment("Set to false to disable Ore").define("enabled", true);
+            this.minY = builder.comment("Min Y level").define("minY", minY);
+            this.maxY = builder.comment("Max Y Level").define("maxY", maxY);
+            this.count = builder.comment("Ore vein count").define("veinCount", count);
+            this.size = builder.comment("Ore vein size").define("veinSize", size);
+        }
     }
 }
